bc1bfa94424643d38fe84ecaaca9364516a3b1f2
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-06-03 15:17:13 -0300

    Add files via upload

--- Act-18_Adrian_Misme/punto 3/script.py
+++ Act-18_Adrian_Misme/punto 3/script.py
@@ -6,15 +6,25 @@ En el bloque principal del programa cargar los lados de dos rectángulos y
 luego mostrar cuál de los dos tiene una superficie mayor.
 """
 
-def retornar_superficie(lado1,lado2):
 
+def retornar_superficie(lado1,lado2):
     sup = lado1*lado2
     return sup
 
+print("rectangulo 1 :")
+l1_rect1=int(input("ingrese valor de Lado1 : "))
+l2_rect1=int(input("ingrese valor de Lado2 : "))
 
-for x in range(2)
-    l1=int(input("ingrese valor de Lado1 : "))
-    l2=int(input("ingrese valor de Lado2 : "))
-    
-retornar_superficie(l1,l2)
+print("rectangulo 2 :")
+l1_rect2=int(input("ingrese valor de Lado1 : "))
+l2_rect2=int(input("ingrese valor de Lado2 : "))
 
+rectangulo_1 = retornar_superficie(l1_rect1,l2_rect1)
+rectangulo_2 = retornar_superficie(l1_rect2,l2_rect2)
+
+ 
+if rectangulo_1 > rectangulo_2:
+    print("El rectangulo 1 es el que tiene mayor superficie")
+
+else:
+    print("El rectangulo 2 es el que tiene mayor superficie")
--- Act-18_Adrian_Misme/punto 4/script.py
+++ Act-18_Adrian_Misme/punto 4/script.py
@@ -2,3 +2,19 @@
 4. Plantear una función que reciba un string en mayúsculas o minúsculas y
 retorne la cantidad de letras &#39;a&#39; o &#39;A&#39;.
 """
+
+def Cantidad_Aa(letra):
+    
+    cant1=letra.count("a")
+    cant2=letra.count("A")
+    cantTotal=cant1+cant2
+    
+    return cantTotal
+
+
+
+texto=input("ingrese cadena de texto : ")
+
+cantidad = Cantidad_Aa(texto)
+
+print("Cantidad de A - a : ", cantidad)
