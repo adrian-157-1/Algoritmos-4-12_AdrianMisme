49e7b2ba57f3c4bf94ebec010f54907480038716
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-06-03 17:37:45 -0300

    Add files via upload

---
+++ Act-19_Adrian_Misme/punto 2/script.py
@@ -0,0 +1,9 @@
+"""
+2. En una empresa se almacenaron los sueldos de 10 personas.
+Desarrollar las siguientes funciones y llamarlas desde el bloque principal:
+1) Carga de los sueldos en una lista.
+2) Impresión de todos los sueldos.
+3) Cuántos tienen un sueldo superior a $4000.
+4) Retornar el promedio de los sueldos.
+5) Mostrar todos los sueldos que están por debajo del promedio.
+"""
