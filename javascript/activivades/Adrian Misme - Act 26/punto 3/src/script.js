d402d8b73462440b482ae69610064aac4b27d207
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-08-25 17:39:35 -0300

    Add files via upload

---
+++ javascript/activivades/Adrian Misme - Act 26/punto 3/src/script.js
@@ -0,0 +1,10 @@
+/* 
+Ejercicio 03: Simulador de Votación en Línea
+Plantear una página con 3 botones, cada uno representando un candidato distinto.
+Al hacer clic en uno de los botones, se deberá aumentar el contador de votos de ese
+candidato y mostrar el total actualizado en pantalla.
+Además:
+ El sistema debe mostrar en consola quién va ganando cada vez que se registra
+un voto.
+ Si hay un empate, debe mostrar el mensaje “Hay un empate”.
+*/
