3d69291321e9386e9eed273414431bb5c226a53d
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-08-26 17:47:55 -0300

    Add files via upload

---
+++ javascript/activivades/Adrian Misme - Act 26/Adrian Misme - Act 26/punto 3/src/script.js
@@ -0,0 +1,74 @@
+/* 
+Ejercicio 03: Simulador de Votación en Línea
+Plantear una página con 3 botones, cada uno representando un candidato distinto.
+Al hacer clic en uno de los botones, se deberá aumentar el contador de votos de ese
+candidato y mostrar el total actualizado en pantalla.
+Además:
+ El sistema debe mostrar en consola quién va ganando cada vez que se registra
+un voto.
+ Si hay un empate, debe mostrar el mensaje “Hay un empate”.
+*/
+
+
+
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+
+let voto1 = document.getElementById("voto1")
+let voto2 = document.getElementById("voto2")
+let voto3 = document.getElementById("voto3")
+
+let cantidad1=0
+let cantidad2=0
+let cantidad3=0
+
+
+
+
+btn1.addEventListener("click", function()
+{
+cantidad1++
+voto1.textContent = cantidad1
+
+}
+)
+
+
+
+btn2.addEventListener("click", function()
+{
+cantidad2++
+voto2.textContent = cantidad2
+
+}
+)
+
+
+
+btn3.addEventListener("click", function()
+{
+cantidad3++
+voto3.textContent = cantidad3
+
+}
+)
+
+if(cantidad1 > cantidad2 && cantidad1 > cantidad3){
+    console.log("primer candidato va GANANDO")
+}
+
+if(cantidad2 > cantidad1 && cantidad2 > cantidad3){
+    console.log("segundo candidato va GANANDO")
+}
+
+if(cantidad3 > cantidad1 && cantidad3 > cantidad2){
+    console.log("tercre candidato va GANANDO")
+}
+
+if(cantidad1 == cantidad2 == cantidad3){
+    console.log("hay un EMPATE")
+}
+
+
+
