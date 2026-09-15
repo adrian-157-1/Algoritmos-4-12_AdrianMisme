85a46103359d4f7226451ff7feb5e790fba48355
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-09-15 17:49:45 -0300

    Add files via upload

---
+++ javascript/activivades/Adrian Misme - Act 29/punto 2/src/script.js
@@ -0,0 +1,13 @@
+/* 
+Ejercicio 2: Carrito de Compras con Conteo de Productos
+Enunciado: Crear un carrito de compras utilizando LocalStorage, que permita a los
+usuarios agregar productos y muestre la cantidad total de productos en el carrito.
+1. Los productos deben tener un botón para agregar al carrito.
+2. Al agregar un producto, se debe mostrar el número total de productos en el
+carrito, almacenándolo en LocalStorage.
+3. Al recargar la página, el número total de productos debe recuperarse de
+LocalStorage y mostrarse correctamente.
+un evento).
+*/
+
+
---
+++ javascript/activivades/Adrian Misme - Act 29/punto 3/src/script.js
@@ -0,0 +1,21 @@
+/* 
+Ejercicio 3: Borrador de Notas de Sesión
+Enunciado: Crear una aplicación de notas rápidas que permita al usuario escribir un
+mensaje o nota en un campo de texto y guardarlo de forma temporal utilizando
+SessionStorage.
+ Debe contar con un campo de texto (&lt;textarea&gt; o &lt;input&gt;) y un botón &quot;Guardar
+Nota&quot;.
+ Al hacer clic en el botón &quot;Guardar Nota&quot;, el texto ingresado debe almacenarse
+en SessionStorage y mostrarse en un elemento dentro del DOM (por ejemplo,
+en un contenedor &lt;div&gt; o párrafo).
+ Al recargar la página (F5), la nota guardada debe recuperarse de
+SessionStorage y seguir mostrándose en pantalla.
+ Debe incluir un botón &quot;Borrar Nota&quot; que elimine el registro de SessionStorage
+mediante removeItem() y limpie el contenido del DOM.
+*/
+
+
+
+
+
+
