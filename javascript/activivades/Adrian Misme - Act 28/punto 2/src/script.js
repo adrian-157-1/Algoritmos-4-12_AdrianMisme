ca9fb52f4b6752a4eed6e5d7782ee3c66a27e38e
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-09-23 00:07:41 -0300

    Delete javascript/activivades/Adrian Misme - Act 28 directory

--- javascript/activivades/Adrian Misme - Act 28/punto 2/src/script.js
+++
@@ -1,6 +0,0 @@
-/* 
-2. Confeccionar una página de visitas a un sitio, solicitar ingresar el nombre de una
-persona, su mail y los comentarios (TEXTAREA). Mostrar luego llamando a la función
-alert los datos ingresados.
-*/
-
