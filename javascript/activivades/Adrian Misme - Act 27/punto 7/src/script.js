0484ce4dc71334492c700bb9b5abe53aba8e94fb
adrian-157-1 <adrianmismepocho7@gmail.com>
2026-09-23 00:09:21 -0300

    Delete javascript/activivades/Adrian Misme - Act 27 directory

--- javascript/activivades/Adrian Misme - Act 27/punto 7/src/script.js
+++
@@ -1,7 +0,0 @@
-/* 
-7. Confeccionar una página que muestre tres checkbox que permitan
-seleccionar los deportes que practica el usuario (Fútbol, Básquet, Tenis)
-Mostrar al presionar un botón los deportes que eligió.
-*/ 
-
-
